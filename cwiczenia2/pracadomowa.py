6c86feb0e121df3c37e7af51ff004487586a6765
KubaloPL <superkubalo123@gmail.com>
2024-10-15 09:32:21 +0200

    zad

---
+++ cwiczenia2/pracadomowa.py
@@ -0,0 +1,7 @@
+# Zadanie 1: Przetwarzanie tekstu i analizy statystyczne
+# Napisz program, który:
+#     Pobiera od użytkownika wieloliniowy tekst.
+#     Wypisuje liczbę słów, zdań (przyjmując, że zdania kończą się kropką), oraz średnią długość słowa (w znakach).
+#     Sprawdza, czy podane słowo występuje w tekście. Jeśli tak, wypisuje liczbę jego wystąpień oraz pozycje (indeksy) w tekście.
+# Podaj tekst: "Python jest fajny. Uczymy się Pythona."
+# Podaj szukane słowo: "Python"
